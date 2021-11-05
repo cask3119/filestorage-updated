287b249aed78a42231491af7c8b4f173caf26643
cask3119 <79370289+cask3119@users.noreply.github.com>
2021-11-05 21:10:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,23 +30,23 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @URS_BOND
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+👥 **Movies Group:** [**Hindi HD Movies**](https://t.me/HindiHdMovies_Group)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Movies Channel:** [**Latest HD Movies**](https://t.me/HindiHdMoviesV3)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @URS_BOND
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+[Subscribe US Now](https://t.me/HindiHdMoviesV3)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
 
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+Send me any file I will give you a permanent Sharable Link , You Can Also Store Multiple Files In Single Link. I Support Channel Also! Check **About Bot** Button.
 """
